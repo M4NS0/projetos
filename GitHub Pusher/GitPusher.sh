87b7cc32e65c8ec81be063fbfd5086119b24d88b
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-07-13 15:47:44 -0300

    Update

--- GitHub Pusher/GitPusher.sh
+++ GitHub Pusher/GitPusher.sh
@@ -9,15 +9,18 @@ yad --width 500 --height 200 --title="GitAutoPusher" \
 --field="Repositório:CB" \
 --field="CRUD:CBE" \
 --field="\n\nSe deseja adicionar algum comentário adicional, escreva abaixo::TXT" \
-"$repName" "$crud" "" > ~/$USR/temp
+"$repName" "$crud" "" > temp
 
-NAME=`cat config | awk -F',' '{print $1}'`
-CRUD=`cat config | awk -F',' '{print $2}'`
-COMMENT=`cat config | awk -F',' '{print $3}'`
+NAME=`cat temp | awk -F',' '{print $1}'`
+CRUD=`cat temp | awk -F',' '{print $2}'`
+COMMENT=`cat temp | awk -F',' '{print $3}'`
 
 cd ~/$USR/$NAME
 git add .
+echo
+git remote get-url origin
+echo
 git commit -m "$CRUD  $COMMENT"
+echo
 git push
 exit
-
