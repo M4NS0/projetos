43e38b5a29bf6f989c3c6af7a654162265cc6b09
Bruno C. Manso <mrbrunomanso@gmail.com>
2020-07-13 15:12:23 -0300

    Update

--- GitHub Pusher/GitPusher.sh
+++ GitHub Pusher/GitPusher.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 repName=$(echo "Workspaces,Matriz.Curricular,Projetos,M4NS0.github.io")
-crud=$(echo "Create,Remove,Update,Delete")
+crud=$(echo "Create,Update,Delete")
 yad --width 500 --height 200 --title="GitAutoPusher" \
 --image marca.png \
---form --separator="," --item-separator="," \
+--form --separator="," \
+--item-separator="," \
 --field="Repositório:CB" \
 --field="CRUD:CBE" \
 --field="\n\nSe deseja adicionar algum comentário adicional, escreva abaixo::TXT" \
@@ -17,10 +18,6 @@ COMMENT=`cat config | awk -F',' '{print $3}'`
 cd ~/$USR/$NAME
 git add .
 git commit -m "$CRUD  $COMMENT"
-git status
 git push
 exit
 
-yad --form --title "GitAutoPusher" --text="\n\n  Foi feito um $CRUD no Repositório $NAME  \n\n"  
-cd ~/$USR/Workspaces/Shell/GitHubPusher
-./GitPusher.sh
